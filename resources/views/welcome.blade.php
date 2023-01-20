341ce385ea60b926ae2c2219dde0e72bcd2a0fbe
CenzSmith18 <70206065+CenzSmith18@users.noreply.github.com>
2023-01-21 00:38:02 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+  <!-- Navbar -->
+  @includeIf('layouts.navbar')
+  <!-- /.navbar -->
+
+  <!-- Section -->
+  @includeIf('layouts.section1')
+  <!-- End Section-->
+
+  <!-- Pesan1 -->
+    @includeIf('layouts.pesan')
+  <!-- End Pesan1-->
+  <!-- Tentang Kami -->
+  @includeIf('layouts.tentangkami')
+  <!-- End Tentang Kami-->
